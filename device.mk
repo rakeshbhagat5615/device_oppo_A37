0ababa5d82038569aee96cab7c0089bdc069268a
Rakesh Bhagat <rakeshbhagat.rb5615@gmail.com>
2022-03-20 14:31:22 +0530

    a37: add camera wrapper from lenovo a6020 and update camera flags

--- device.mk
+++ device.mk
@@ -173,8 +173,12 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
-    Camera2
+    camera.msm8916 \
+    Camera2 \
+    Snap \
+    SnapdragonCamera
 
 # Permissions
 PRODUCT_COPY_FILES += \
